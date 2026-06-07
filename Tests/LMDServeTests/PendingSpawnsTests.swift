32fe25dbf88d3aa5d4f5920768574f037e27e470
Alexander Goodkind <alex@goodkind.io>
2026-06-07 16:31:21 -0700

    Reformat sources with swift-format

--- Tests/LMDServeTests/PendingSpawnsTests.swift
+++ Tests/LMDServeTests/PendingSpawnsTests.swift
@@ -1,4 +1,5 @@
 import XCTest
+
 @testable import LMDServeSupport
 
 final class PendingSpawnsTests: XCTestCase {
--- Tests/SwiftLMHostProtocolTests/FramesTests.swift
+++ Tests/SwiftLMHostProtocolTests/FramesTests.swift
@@ -1,4 +1,5 @@
 import XCTest
+
 @testable import SwiftLMHostProtocol
 
 final class FramesTests: XCTestCase {
