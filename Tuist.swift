a973f6a81dcdef931408a9664f205e329c292487
Alexander Goodkind <alex@goodkind.io>
2026-06-27 12:21:46 -0700

    Add file headers and a named constant to satisfy the hard lint gate in Tuist manifests

--- Tuist.swift
+++ Tuist.swift
@@ -1,3 +1,11 @@
+//
+//  Tuist.swift
+//  lmd
+//
+//  Created by Alexander Goodkind <alex@goodkind.io> on 2026-05-10.
+//  Copyright © 2026, all rights reserved.
+//
+
 import ProjectDescription
 
 let tuist = Tuist()
