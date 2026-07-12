e66c9cf07175237343e9fe4ce07c6ae7c3fdeee4
Alex Goodkind <alex@goodkind.io>
2026-07-12 10:02:02 -0700

    Pin mlx-swift-lm to the logprob build and unify mlx-swift forks (#45)

--- Package.swift
+++ Package.swift
@@ -44,7 +44,7 @@ let package = Package(
     .package(url: "https://github.com/Quick/Nimble.git", from: "14.0.0"),
     .package(
       url: "https://github.com/agoodkind/mlx-swift-lm.git",
-      revision: "1869eaa3f0ae86fcd440ff67fe1b0988e00a787b"),
+      revision: "9d77504276de8113b1f5be3a519c2b9d35938ebc"),
     .package(url: "https://github.com/agoodkind/mlx-swift.git", branch: "main"),
     .package(url: "https://github.com/huggingface/swift-huggingface.git", from: "0.9.0"),
     .package(url: "https://github.com/huggingface/swift-transformers.git", from: "1.3.2"),
